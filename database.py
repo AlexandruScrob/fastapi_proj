f7185c69348cd68b991d100771e2cdc03ff408f2
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-09-14 22:49:48 +0300

    wip

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False})
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+
+Base = declarative_base()
+
+print("DB exists: ", database_exists(engine.url))
+
--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from fastapi import FastAPI
+import models
+
+from database import engine, SessionLocal
+
+from fastapi import FastAPI, Depends
+from schemas import Blog
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return 'hello'
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()
 
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+@app.post('/blog')
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return new_blog
+
+
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{_id}')
+def get_single(_id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == _id).first()
+    return blog
